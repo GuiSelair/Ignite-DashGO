b55b73b7dc9fd0f7a315c0f4ab2e31c33b4302bf
Guilherme Selair <guilherme.lima@getrak.com.br>
2022-07-26 01:02:32 -0300

    Feat: Added React-Query

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,26 +13,31 @@ import {
 	Td,
 	Text,
 	useBreakpointValue,
+	Spinner,
+	Alert,
+	AlertIcon,
+	AlertDescription,
 } from "@chakra-ui/react";
 import { RiAddLine } from "react-icons/ri";
 import Link from "next/link";
+import { useQuery } from "@tanstack/react-query";
 
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import Sidebar from "../../components/Sidebar";
-import { useEffect } from "react";
 
 export default function UserList() {
+	const { isLoading, data, error } = useQuery(["users"], async () => {
+		const responseRaw = await fetch("http://localhost:3000/api/dev/users");
+		const responseData = await responseRaw.json();
+
+		return responseData;
+	});
+
 	const isWideVersion = useBreakpointValue({
 		base: false,
 		lg: true,
 	});
-
-	useEffect(() => {
-		fetch("http://localhost:3000/api/dev/users")
-			.then((data) => data.json())
-			.then((response) => console.log(response));
-	}, []);
 
 	return (
 		<Box>
@@ -60,34 +65,51 @@ export default function UserList() {
 						</Link>
 					</Flex>
 
-					<Table colorScheme={"whiteAlpha"}>
-						<Thead>
-							<Tr>
-								<Th px={["4", "4", "6"]} color={"gray.300"} width="8">
-									<Checkbox colorScheme={"pink"} />
-								</Th>
-								<Th>Usuário</Th>
-								{isWideVersion && <Th>Data de cadastro</Th>}
-							</Tr>
-						</Thead>
-						<Tbody>
-							<Tr>
-								<Td px={["4", "4", "6"]}>
-									<Checkbox colorScheme={"pink"} />
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight={"bold"}>Guilherme Selair</Text>
-										<Text fontSize={"sm"} color="gray.300">
-											guilherme.selair@gmail.com
-										</Text>
-									</Box>
-								</Td>
-								{isWideVersion && <Td>13 de Julho de 2022</Td>}
-							</Tr>
-						</Tbody>
-					</Table>
-					<Pagination />
+					{isLoading ? (
+						<Flex justify={"center"}>
+							<Spinner />
+						</Flex>
+					) : error ? (
+						<Flex justify={"center"}>
+							<Alert status="error">
+								<AlertIcon />
+								<AlertDescription>
+									Falha ao obter dados do usuários
+								</AlertDescription>
+							</Alert>
+						</Flex>
+					) : (
+						<>
+							<Table colorScheme={"whiteAlpha"}>
+								<Thead>
+									<Tr>
+										<Th px={["4", "4", "6"]} color={"gray.300"} width="8">
+											<Checkbox colorScheme={"pink"} />
+										</Th>
+										<Th>Usuário</Th>
+										{isWideVersion && <Th>Data de cadastro</Th>}
+									</Tr>
+								</Thead>
+								<Tbody>
+									<Tr>
+										<Td px={["4", "4", "6"]}>
+											<Checkbox colorScheme={"pink"} />
+										</Td>
+										<Td>
+											<Box>
+												<Text fontWeight={"bold"}>Guilherme Selair</Text>
+												<Text fontSize={"sm"} color="gray.300">
+													guilherme.selair@gmail.com
+												</Text>
+											</Box>
+										</Td>
+										{isWideVersion && <Td>13 de Julho de 2022</Td>}
+									</Tr>
+								</Tbody>
+							</Table>
+							<Pagination />
+						</>
+					)}
 				</Box>
 			</Flex>
 		</Box>
